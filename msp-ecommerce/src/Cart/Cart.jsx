c9332bcc1f124fb03740b0802cd46708d91eb6d4
NesmaFarid9 <119267221+NesmaFarid9@users.noreply.github.com>
2023-07-28 01:09:54 +0300

    Update Cart.jsx

--- msp-ecommerce/src/Cart/Cart.jsx
+++ msp-ecommerce/src/Cart/Cart.jsx
@@ -1,141 +1,47 @@
 import React, { Fragment } from 'react'
+import { Cart3 } from 'react-bootstrap-icons';
 import style from "./Cart.module.css"
-// import images
-import image1 from "../images/gloves.jpg";
-import image2 from "../images/camera.jpg";
-import image3 from "../images/t-shirts.jpg";
-import image4 from "../images/pants.jpg";
-import image5 from "../images/dress.jpg";
-import image6 from "../images/shoes.jpg";
-import image7 from "../images/bag.jpg";
-import image8 from "../images/hat.jpg";
-import image9 from "../images/sunglasses.jpg";
-import image10 from "../images/lamp.jpg";
-import image11 from "../images/towel.jpg";
-import image12 from "../images/chairs.jpg";
-import image13 from "../images/cushion.jpg";
-import image14 from "../images/coffeeCups.jpg";
-import image15 from "../images/curtain.jpg";
-
-
-const Cart = () => {
+const Cart = ({ list, RemoveFromCart }) => {
     return (
         <Fragment>
-            <div className={style.mainCart}>
-                <div>
-                    <div className={style.image} style={{ backgroundImage: `url(${image13})` }}></div>
-                    <div className={style.details}>
-                        <h3>Product</h3>
-                    
-                        <div className={style.cartFooter}>
-
-                            <div className={style.price}>
-                                <span>
-                                    <sub>EGP</sub>
-                                    <p>500</p>
-                                </span>
-                                <span>
-                                    <del>150</del>
-                                    <p>30 % off</p>
-                                </span>
-
-                            </div>
-                            <button>Remove</button>
-                        </div>
-                    </div>
+            <div className={style.cartIconContiner}>
+                <div className={style.continer}>
+                    <Cart3 className={style.cartIcon} />
+                    <p>{list.length}</p>
                 </div>
-                <div className={style.hr}></div>
-                <div>
-                    <div className={style.image}></div>
-                    <div className={style.details}>
-                        <h3>Product</h3>
-                        <div className={style.cartFooter}>
-
-                            <div className={style.price}>
-                                <span>
-                                    <sub>EGP</sub>
-                                    <p>500</p>
-                                </span>
-                                <span>
-                                    <del>150</del>
-                                    <p>30 % off</p>
-                                </span>
-
-                            </div>
-                            <button>Remove</button>
-                        </div>
+                <main>
+                    <div className={style.mainCart}>
+                        {
+                            list.map(({ image, name, oldPrice, offer},ind) => (
+                                <Fragment key={ind}>
+                                    <div>
+                                        <div className={style.image} style={{ backgroundImage: `url(${image})` }}></div>
+                                        <div className={style.details}>
+                                            <h3>{name}</h3>
+                                            <div className={style.cartFooter}>
+                                                <div className={style.price}>
+                                                    <span>
+                                                        <sub>EGP</sub>
+                                                        <p>{(oldPrice * (offer / 100)).toFixed(2)}</p>
+                                                    </span>
+                                                    <span>
+                                                        <del>{oldPrice}</del>
+                                                        <p>{offer} % off</p>
+                                                    </span>
+                                                </div>
+                                                <button onClick={(e)=>RemoveFromCart(e,ind)}>Remove</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={style.hr}></div>
+                                </Fragment>
+                            ))
+                        }
                     </div>
-                </div>
-                <div className={style.hr}></div>
-                <div>
-                    <div className={style.image}></div>
-                    <div className={style.details}>
-                        <h3>Product</h3>
-                        <div className={style.cartFooter}>
-
-                            <div className={style.price}>
-                                <span>
-                                    <sub>EGP</sub>
-                                    <p>500</p>
-                                </span>
-                                <span>
-                                    <del>150</del>
-                                    <p>30 % off</p>
-                                </span>
-
-                            </div>
-                            <button>Remove</button>
-                        </div>
-                    </div>
-                </div>
-                <div className={style.hr}></div>
-                <div>
-                    <div className={style.image}></div>
-                    <div className={style.details}>
-                        <h3>Product</h3>
-                        <div className={style.cartFooter}>
-
-                            <div className={style.price}>
-                                <span>
-                                    <sub>EGP</sub>
-                                    <p>500</p>
-                                </span>
-                                <span>
-                                    <del>150</del>
-                                    <p>30 % off</p>
-                                </span>
-
-                            </div>
-                            <button>Remove</button>
-                        </div>
-                    </div>
-                </div>
-                <div className={style.hr}></div>
-                <div>
-                    <div className={style.image}></div>
-                    <div className={style.details}>
-                        <h3>Product</h3>
-                        <div className={style.cartFooter}>
-
-                            <div className={style.price}>
-                                <span>
-                                    <sub>EGP</sub>
-                                    <p>500</p>
-                                </span>
-                                <span>
-                                    <del>150</del>
-                                    <p>30 % off</p>
-                                </span>
-
-                            </div>
-                            <button>Remove</button>
-                        </div>
-                    </div>
-                </div>
-                <div className={style.hr}></div>
+                </main>
             </div>
         </Fragment>
-    )
-}
+    );
+};
 
-export default Cart
+export default Cart;
